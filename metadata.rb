fc80021943298622c87d1ea9f64a599d164fba5c
Andreas Ruck <mychiara@gmail.com>
2015-04-08 13:50:00 +0200

    version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mychiara+cookbooks@gmail.com'
 license          'MIT'
 description      'Installs/Configures composer-wordpress'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.0.1'
 
 dependencies = ['composer', 'openssl', 'database']
 
