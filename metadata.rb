d4f5d14e2de7f6c090f37b3ce583286d3aea0434
Andreas Ruck <mychiara@gmail.com>
2015-04-17 13:50:27 +0200

    removed source_url because it will lead to errors

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'MIT'
 description      'Installs/Configures composer-wordpress'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
-source_url       'https://github.com/mychiara/composer-wordpress'
+#source_url       'https://github.com/mychiara/composer-wordpress'
 
 depends 'composer',     '>= 2.0.0'
 depends 'openssl',      '>= 4.0.0'
