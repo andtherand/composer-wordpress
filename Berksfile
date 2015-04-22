37bc82c5239508044be6b61628e66256f8614a94
Andy Ruck <mychiara@gmail.com>
2015-04-22 13:53:49 +0200

    kitchen stuff and db recipe

--- Berksfile
+++ Berksfile
@@ -5,7 +5,7 @@ source "https://supermarket.chef.io"
 
 metadata
 
-cookbook 'my-lamp', '~> 0.1.3', github: 'mychiara/my-lamp'
+cookbook 'my-lamp', '~> 1.0.0', github: 'mychiara/my-lamp'
 cookbook 'my_utils_packages', github: 'mychiara/my-utils-packages'
 cookbook 'xdebug', github: 'mychiara/xdebug'
 cookbook 'php-intl', github: 'mychiara/php-intl'
--- recipes/_db.rb
+++ recipes/_db.rb
@@ -21,11 +21,11 @@ end
 
 # create the user and grant rights
 mysql_database_user db['user'] do
-  connection  mysql_connection_info
-  password    db['password']
-  privileges  db['privileges']
-  database    db['name']
-  action      [:create, :grant]
+  connection      mysql_connection_info
+  password        db['password']
+  privileges      db['privileges']
+  database_name   db['name']
+  action          [:create, :grant]
 end
 
 
