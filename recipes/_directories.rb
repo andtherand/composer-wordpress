353c25f12bdeab4adb45c2366f0523f99d68ea33
Andy Ruck <mychiara@gmail.com>
2015-04-23 10:12:16 +0200

    added uploads folder creation as well as setting conditional attributes

--- recipes/_directories.rb
+++ recipes/_directories.rb
@@ -7,34 +7,45 @@
 root_dir  = node['wp_composer']['root_dir']
 doc_root  = "#{root_dir}/#{node['wp_composer']['public_folder']}"
 log_dir   = "#{root_dir}/#{node['wp_composer']['log_dir']}"
+content_folder = "#{doc_root}/#{node['wp_composer']['content_folder']}"
+uploads_folder = "#{content_folder}/uploads"
 
 dirs = [
   "#{doc_root}/#{node['wp_composer']['install_folder']}",
-  "#{doc_root}/#{node['wp_composer']['content_folder']}",
+  content_folder,
   log_dir
 ]
 
 dirs.each do | dir |
-  unless File.directory?(dir)
-    # installation folder
-    directory dir do
-      owner     'root'
-      group     'root'
-      mode      '0755'
-      recursive true
-      action    :create
-    end
+  # installation folder
+  directory dir do
+    owner     'root'
+    group     'root'
+    mode      '0755'
+    recursive true
+    action    :create
+
+    not_if File.directory?(dir)
   end
 end
 
+directory uploads_folder  do
+  owner   node['apache']['user']
+  group   node['apache']['group']
+  mode    '0755'
+  action  :create
+
+  not_if File.directory?(uploads_folder)
+end
+
 ["#{log_dir}/error.log", "#{log_dir}/access.log"].each do | log_file |
-  unless File.exist?(log_file)
-    file log_file do
-      owner   'root'
-      group   'root'
-      mode    '0644'
-      action  :create
-    end
+  file log_file do
+    owner   'root'
+    group   'root'
+    mode    '0644'
+    action  :create
+
+    not_if File.exist?(log_file)
   end
 end
 
